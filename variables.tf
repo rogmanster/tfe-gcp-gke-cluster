280abd083b9efb64d86cad34cc0bd5180abcd03f
Roger Chao <rogman@gmail.com>
2020-05-29 09:56:58 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_credentials" {}
+variable "gcp_region" {}
+variable "gcp_zone" {}
+variable "gcp_project" {}
+variable "cluster_name" {}
+variable "initial_node_count" {}
+variable "master_username" {}
+variable "master_password" {}
+variable "node_machine_type" {}
+variable "node_disk_size" {}
+variable "environment" {}
